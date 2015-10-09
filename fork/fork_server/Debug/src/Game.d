3384ae5a9469f3af5d81164097c92d09273e1ae3
David Morton <davidmortonmail@gmail.com>
2015-10-09 07:38:13 +0100

    added libsocket for osx named *.dylib

--- fork/fork_server/Debug/src/Game.d
+++ fork/fork_server/Debug/src/Game.d
@@ -1,5 +1,5 @@
 src/Game.d: ../src/Game.c ../src/../includes/Definitions.h \
- ../src/../includes/Game.h ../src/../words
+  ../src/../includes/Game.h ../src/../words
 
 ../src/../includes/Definitions.h:
 
--- fork/fork_server/Debug/src/hangserver_iter.d
+++ fork/fork_server/Debug/src/hangserver_iter.d
@@ -1,6 +1,6 @@
 src/hangserver_iter.d: ../src/hangserver_iter.c \
- ../src/../includes/Definitions.h ../src/../../../libsocket/Sockets.h \
- ../src/../includes/Game.h
+  ../src/../includes/Definitions.h ../src/../../../libsocket/Sockets.h \
+  ../src/../includes/Game.h
 
 ../src/../includes/Definitions.h:
 
