3240bc92e61047e6a8eab9ad2b79fdbfb4008722
Derek O'Brien <obrien.derek1@gmail.com>
2015-12-01 13:37:11 +0000

    Fixing warnings on select build

--- select/select_client/src/hangclient.c
+++ select/select_client/src/hangclient.c
@@ -29,7 +29,7 @@ int main(int argc, char * argv[]) {
 	Write(iSocketFileDescriptor, &buffer, strlen(buffer) +1);
 	printf("Username %s sent to the server\n", buffer);
 
-	MultiplexStdinFileDescriptor(stdin, iSocketFileDescriptor);
+	MultiplexIO(stdin, iSocketFileDescriptor);
 
 	printf("Game over");
 	return 0;
