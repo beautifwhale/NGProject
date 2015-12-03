72170ef0e6b0d0be1f3bbab794367b88ab136b05
David Morton <davidmortonmail@gmail.com>
2015-12-03 15:53:42 +0000

    removing debug folder

---
+++ udp/udp_client/Debug/sources.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+build/CMakeFiles/3.4.0-rc2/CompilerIdC \
+build/CMakeFiles/clientUDP.dir/src \
+build/CMakeFiles \
+build/libhangman/src/libhangman-build/CMakeFiles/3.4.0-rc2/CompilerIdC \
+build/libhangman/src/libhangman-build/CMakeFiles \
+build/libhangman/src/libhangman-build/CMakeFiles/hangman.dir/src \
+build/libhangman/src/libhangman-build/libsocket/src/libsocket-build/CMakeFiles/3.4.0-rc2/CompilerIdC \
+build/libhangman/src/libhangman-build/libsocket/src/libsocket-build/CMakeFiles \
+build/libhangman/src/libhangman-build/libsocket/src/libsocket-build/CMakeFiles/socket.dir/src \
+external/libhangman/external/libsocket/src \
+external/libhangman/src \
+src \
+
---
+++ udp/udp_client/Debug/src/udp_client.d
@@ -0,0 +1,7 @@
+src/udp_client.o: ../src/udp_client.c \
+  ../src/../external/libhangman/include/hangman.h \
+  ../src/../external/libhangman/include/../external/libsocket/include/socket.h
+
+../src/../external/libhangman/include/hangman.h:
+
+../src/../external/libhangman/include/../external/libsocket/include/socket.h:
