f979aaf80bd72e8835f6065565a0536cee630c32
Derek O'Brien <obrien.derek1@gmail.com>
2015-12-03 11:07:23 +0000

    Update select_client.c

--- select/select_client/src/select_client.c
+++ select/select_client/src/select_client.c
@@ -3,9 +3,12 @@
 // Year 4 Networked Games Assignment 2015
 // Team:	David Morton
 //			Kevin Byrne
+//			Derek O Brien
 // 			add names here...
 //
-// Description:
+// Description: The Select client will take the username and then start a connection with the server
+// using the once the connection is made the client uses MultiplexIO() function which implements 
+// select to check if the input is from the stdin or the server.
 //
 // Dependencies: libhangman, libsocket
 //
@@ -24,12 +27,15 @@ int main(int argc, char * argv[]) {
 
 	strServerIPAddress = argv[1];
 
+	//Set up socket and connection
 	iSocketFileDescriptor = Socket(AF_INET, SOCK_STREAM, 0);
-
+	
 	Address(AF_INET, (struct Address*) &sAddress, strServerIPAddress, HANGMAN_TCP_PORT);
 
 	Connect(iSocketFileDescriptor, (struct sockaddr*) &sAddress.m_sAddress, sizeof(sAddress.m_sAddress));
 
+	// Get the username from stdin
+	// Send username to the server
 	sprintf(buffer, "%s ", username);
 	Write(iSocketFileDescriptor, &buffer, strlen(buffer) +1);
 	printf("Username %s sent to the server\n", buffer);
